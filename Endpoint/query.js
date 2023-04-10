5da82996c251c155a82ea7ca42e80518d405f4b9
amyria3 <54778894+amyria3@users.noreply.github.com>
2023-04-10 12:08:02 +0200

    -

--- Endpoint/query.js
+++ Endpoint/query.js
@@ -1,18 +1,18 @@
-import { PrismaClient } from "@prisma/client";
+  import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+  const prisma = new PrismaClient();
 
-export default async function all() {
-  const elements = await prisma.typesOfRessource.findMany();
-  console.log(elements);
-}
+  export default async function all() {
+    const elements = await prisma.typesOfRessource.findMany();
+    console.log(elements);
+  }
 
-all()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+  all()
+    .then(async () => {
+      await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+      console.error(e);
+      await prisma.$disconnect();
+      process.exit(1);
+    });
