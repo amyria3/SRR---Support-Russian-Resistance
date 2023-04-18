50c8b2c7c16cf392326fe809d1af9f0ac7001987
amyria3 <54778894+amyria3@users.noreply.github.com>
2023-04-18 17:34:38 +0200

    unimportant changes

--- src/server/query.js
+++ src/server/query.js
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function askPrisma() {
-  console.log("asking Prisma");
+  console.log("query.js, asking Prisma");
   const elements = await prisma.ngo.findMany({
     orderBy: {
       id: 'asc'
@@ -12,7 +12,7 @@ async function askPrisma() {
       allLinkedKeywords: true
     }
   });
-  console.log("asked Prisma");
+  console.log("query.js, line 15" + typeof(elements) + JSON.stringify(elements))
   return elements;
 }
 
