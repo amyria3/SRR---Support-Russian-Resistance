7dd87fbe93bdf2b2c44f8c8a76cd9f1d4442e7fb
amyria3 <54778894+amyria3@users.noreply.github.com>
2023-04-20 23:45:30 +0200

    search works.

---
+++ seedDocs/rawData.js
@@ -0,0 +1,57 @@
+const radData = [
+  {
+    id: "1bb1b007-784b-43dc-b2db-37ecc2f4f839",
+    name: "Nowaya Gazeta",
+    description:
+      "Nowaya Gazeta is an independent investigative newspaper. It is published in Moscow, in regions within Russia, and in some foreign countries. (Wikipedia, 14.04.2023)",
+    img_url: "",
+    allResources: [
+      {
+        id: "ab7f868b-8db6-4cc3-9ad9-0d034e42e363",
+        url: "https://nowayagazeta.ru/",
+        description: null,
+        ngoId: "1bb1b007-784b-43dc-b2db-37ecc2f4f839",
+        resourceType: "Webpage",
+      },
+      {
+        id: "8cc12a21-9b6e-4952-922b-47ac732ebffb",
+        url: "https://www.instagram.com/novayagazeta/",
+        description: null,
+        ngoId: "1bb1b007-784b-43dc-b2db-37ecc2f4f839",
+        resourceType: "Instagram",
+      },
+      {
+        id: "f74f444b-26d4-4637-814f-6be330c11d2e",
+        url: "https://t.me/novaya_pishet",
+        description: null,
+        ngoId: "1bb1b007-784b-43dc-b2db-37ecc2f4f839",
+        resourceType: "Telegram",
+      },
+    ],
+    allLinkedKeywords: [
+      {
+        ngoId: "1bb1b007-784b-43dc-b2db-37ecc2f4f839",
+        protoKeyword: "Emotional assistance",
+        id: "c8e77502-7ab0-4d0f-8bd6-2764fbb46365",
+      },
+    ],
+  },
+  {
+    id: "bf812916-539c-4005-966f-b82b990adddf",
+    name: "Another Dummy record",
+    description: "Lorem Ipsum, 2001",
+    img_url: "",
+    allResources: [],
+    allLinkedKeywords: [],
+  },
+  {
+    id: "f4151a31-7523-477d-b7a4-6efd369067e5",
+    name: "some name",
+    description: null,
+    img_url: "",
+    allResources: [],
+    allLinkedKeywords: [],
+  },
+];
+
+export default radData;
