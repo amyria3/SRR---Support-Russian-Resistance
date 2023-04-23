f247f4b9e74589ddd331863a46bc17d2fa9eba21
amyria3 <54778894+amyria3@users.noreply.github.com>
2023-04-23 19:00:34 +0200

    combined search works! Yes!

--- src/assets/search/search.js
+++ src/assets/search/search.js
@@ -2,29 +2,20 @@ import lunr from "lunr";
 
 function search(data, indexa, term) {
   const results = [];
-
-  console.log("Testing search script : ");
-  console.log("Term " + term);
+  const { idx, resourceIdx, keywordIdx } = indexa;
 
   //PREFIX MATCHES
-
-
-
-  //EXACT MATCHES
-  ((data, indexa, term) => {
-
-    console.log("Inline function running.")
-
-    const { idx, resourceIdx, keywordIdx } = indexa;
+  if (term.length > 0 && term.length <= 5) {
+    const query = term + "*";
 
     // Search top-level fields
-    const idxResults = idx.search(term);
+    const idxResults = idx.search(query);
     idxResults.forEach((result) => {
       results.push(data.find((doc) => doc.id === result.ref));
     });
 
     // Search resources
-    const resourceResults = resourceIdx.search(term);
+    const resourceResults = resourceIdx.search(query);
     resourceResults.forEach((result) => {
       data.forEach((doc) => {
         doc.allResources.forEach((resource) => {
@@ -36,7 +27,7 @@ function search(data, indexa, term) {
     });
 
     // Search keywords
-    const keywordResults = keywordIdx.search(term);
+    const keywordResults = keywordIdx.search(query);
     keywordResults.forEach((result) => {
       data.forEach((doc) => {
         doc.allLinkedKeywords.forEach((keyword) => {
@@ -46,7 +37,57 @@ function search(data, indexa, term) {
         });
       });
     });
-  })(data, indexa, term);
+  }
+
+  if (term.length > 4) {
+    // Search top-level fields
+    const idxResults = idx.query(function (q) {
+      q.term(lunr.tokenizer(query), {
+        boost: 10,
+        editDistance: 3,
+        prefix: query.length,
+      });
+    });
+    idxResults.forEach((result) => {
+      results.push(data.find((doc) => doc.id === result.ref));
+    });
+
+    // Search resources
+    const resourceResults = resourceIdx.query(function (q) {
+      q.term(lunr.tokenizer(query), {
+        boost: 3,
+        editDistance: 2,
+        prefix: query.length,
+      });
+    });
+    resourceResults.forEach((result) => {
+      data.forEach((doc) => {
+        doc.allResources.forEach((resource) => {
+          if (resource.id === result.ref) {
+            results.push(doc);
+          }
+        });
+      });
+    });
+
+    // Search keywords
+    const keywordResults = keywordIdx.query(function (q) {
+      q.term(lunr.tokenizer(query), {
+        boost: 7,
+        editDistance: 2,
+        prefix: query.length,
+      });
+    });
+    keywordResults.forEach((result) => {
+      data.forEach((doc) => {
+        doc.allLinkedKeywords.forEach((keyword) => {
+          if (keyword.id === result.ref) {
+            results.push(doc);
+          }
+        });
+      });
+    });
+  }
 
   // Remove duplicate results
   const uniqueResults = Array.from(new Set(results));
