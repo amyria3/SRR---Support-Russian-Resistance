0f31209c911cca6578674fb840d93cac9fac6ca1
amyria3 <54778894+amyria3@users.noreply.github.com>
2023-05-11 19:42:13 +0200

    changed 'create' to 'upsert'

--- seedDocs/seedTag.js
+++ seedDocs/seedTag.js
@@ -1,16 +1,18 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-export default async function seedTag(data, boolean) {
+export default async function seedTag(data) {
   for (const entry of data) {
     console.log(entry);
-    const existingEntry = await prisma.ResourceTag.create({
-      data: {
+    const createdEntry = await prisma.ResourceTag.upsert({
+      where: {
         name: entry.name,
       },
+      create: {
+        name: entry.name,
+      },
+      update: {
+      }
     });
-
-    boolean = true
-    console.log(boolean)
   }
 }
