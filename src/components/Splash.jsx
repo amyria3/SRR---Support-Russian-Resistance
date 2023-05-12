cb6351c4712b5f980d4b84752a084d8f77b4078a
amyria3 <54778894+amyria3@users.noreply.github.com>
2023-05-12 18:50:50 +0200

    updated text

--- src/components/Splash.jsx
+++ src/components/Splash.jsx
@@ -3,7 +3,11 @@ function Splash() {
     <div id="splash" className="flex flex-col items-start pt-4 gap-[30px]">
       <h1 className="text-6xl font-light dark:font-extralight mt-12 relative right-1">Human rights & anti-war movement</h1>
       <p className="w-full relative text-right bottom-7"><img src="../assets/illustrations/comment.svg" alt="" /></p>
-      <p className="text-lg dark:text-base font-extralight leading-5 dark:leading-6 mb-10">Alle linksliberalen bzw. Menschenrechtsorganisationen in Russland sind Druck und Repressionen ausgesetzt. Die Verschlechterung der wirtschaftlichen Situation infolge des Krieges geht auch an der Zivilgesellschaft nicht spurlos vorbei. Gleichzeitig haben einige Medien und NGOs aufgrund der inzwischen flächendeckend drohenden Gefahr für Freiheit und Leben ihre Arbeit eingeschränkt oder eingestellt: wieder viele Andere machen unter den erschwerten Bedingungen weiter. Oft sind es insbesondere Frauen, die sich weiter für den Protest entscheiden. Sie riskieren ihre Gesundheit, ihre körperliche Unversehrtheit, ihre Perspektiven in Bildung und Beruf. Sie brauchen unsere Unterstützung. Besonders gefährlich ist der antimilitaristische Protest und jegliche Solidarität mit Ukrainerinnen und Ukrainern. </p>
+      <p className="text-lg dark:text-base font-extralight leading-5 dark:leading-6 mb-10 max-w-[940px]">
+      A couple of people (mostly with Russian or Russian background) started this website because many people  felt the need to do something against the war.
+      <br /><br />For years, left-liberal and human rights organizations in Russia have been subjected to strong pressure and repression. For the past year, this has reached a new level of escalation, so that some media and NGOs have either gone abroad, or have restricted or stopped their work due to the now widespread threat to freedom and life.
+      <br /><br />Still many others continue under the aggravated conditions. Often it is women in particular who continue to choose to protest. They risk their health, their physical integrity, their perspectives in education and profession.
+      <br /><br />Anti-militarist protest and any solidarity with Ukrainians is particularly dangerous. Even if civil protest alone cannot be enough to stop a mafia in possession of an army, they do help to maintain a perspective and somewhat limit the government's otherwise hardly limited room for maneuver. </p>
     </div>
   );
 }
